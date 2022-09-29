01ec0fcc05c97c7fb2aa423988b3da0c6dfabd2f
leandrobattaglia <leand79bat@gmail.com>
2022-09-29 20:30:04 -0300

    agrege lo que subio la profe al slack

--- clase6_26sep/ejercicioEscuela.js
+++ clase6_26sep/ejercicioEscuela.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var readlineSync = require("readline-sync");
+//   import * as readFileSync from 'readline-sync'
 var Alumno = /** @class */ (function () {
     function Alumno(nombre, nota, DNI) {
         this.nombre = nombre;
@@ -30,6 +30,9 @@ var Profesor = /** @class */ (function () {
     Profesor.prototype.getListaAlumnos = function () {
         return this.listaAlumnos;
     };
+    Profesor.prototype.mostrarAlumnos = function () {
+        console.log(this.listaAlumnos);
+    };
     return Profesor;
 }());
 var Escuela = /** @class */ (function () {
@@ -45,15 +48,15 @@ var Escuela = /** @class */ (function () {
     Escuela.prototype.getListaProfes = function () {
         return this.listaProfes;
     };
-    Escuela.prototype.setProfesor = function (profe) {
-        this.listaProfes.push(profe);
-    };
     return Escuela;
 }());
+//instalar npm install @types/node
+// creamos un gestor que nos permite leer un archivo de texto
 var GestorDeArchivos = /** @class */ (function () {
     function GestorDeArchivos(txtFileLocation) {
         var archivoTxt = fs.readFileSync(txtFileLocation, 'utf-8');
         this.arregloString = archivoTxt.split(';'); //vamos a tener nuestro "objetos" separados por ;
+        //[Juan Perez,333333333,Karen Simari,22222222]
     }
     GestorDeArchivos.prototype.mostrarArreglo = function () {
         console.log(this.arregloString);
@@ -63,13 +66,13 @@ var GestorDeArchivos = /** @class */ (function () {
     };
     return GestorDeArchivos;
 }());
+//funcion para crear un nuevo profe
 function crearProfesor(profesor, arrayProfesor, arrayAlumnos) {
     //transformo el elemento de tipo string en un objeto de tipo Profesor
     var propiedadProfe = profesor.split(','); //[Juan Perez, 333333333, Karen Simari, 22222222]
     var nombre = propiedadProfe[0];
     var DNI = Number(propiedadProfe[1]);
     var listaAlumnos = arrayAlumnos;
-    //creo el objeto nuevoProfesor de la clase Profesor y les paso sus atributos
     var nuevoProfe = new Profesor(nombre, DNI, listaAlumnos);
     //inserto el elemento de tipo Profesor en el arreglo recibido
     arrayProfesor.push(nuevoProfe);
@@ -78,16 +81,17 @@ function crearProfesor(profesor, arrayProfesor, arrayAlumnos) {
 //instalamos readline-sync -- npm install readline-sync
 function borrarrProfe(arregloProfe, position, arregloAlumnos) {
     //editar un profesor en el arreglo
-    var nombre = readlineSync.question("Ingrese el nombre: ");
-    var DNI = Number(readlineSync.question("Ingrese el DNI: "));
-    var listaAlumnos = arrayAlumnos;
-    var nuevoProfe = new Profesor(nombre, DNI, listaAlumnos);
+    // let nombre: string = readlineSync.question("Ingrese el nombre: ")
+    // let DNI: number = Number(readlineSync.question("Ingrese el DNI: "));
+    // let listaAlumnos: Array<Alumno> = arregloAlumnos;
+    // let nuevoProfe: Profesor = new Profesor(nombre,DNI,listaAlumnos);
     delete arregloProfe[position];
-    arregloProfe[position] = nuevoProfe;
+    // arregloProfe[position] = nuevoProfe;
 }
 //Inicio programa
 var datos = new GestorDeArchivos('profes.txt');
 var arrayProfe = [];
+datos.mostrarArreglo();
 var alumno1 = new Alumno('Gloria Dominguez', 8, 252546346);
 var alumno2 = new Alumno('Juan Dominguez', 5, 25254255);
 alumno1.estaAprobado();
@@ -99,3 +103,4 @@ for (var i = 0; i < datos.getArregloString().length; i++) {
     crearProfesor(datos.getArregloString()[i], arrayProfe, arrayAlumnos);
 }
 console.log(arrayProfe);
+arrayProfe[1].mostrarAlumnos();
--- clase6_26sep/ejercicioEscuela.ts
+++ clase6_26sep/ejercicioEscuela.ts
@@ -1,23 +1,24 @@
-import * as fs from 'fs';
-import * as readlineSync from 'readline-sync';
+ import * as fs from 'fs';
+//   import * as readFileSync from 'readline-sync'
+
 
 class Alumno {
-    private nombre: string ;
-    private nota: number; 
+    private nombre: string;
+    private nota: number;
     private DNI: number;
 
-    public constructor(nombre: string, nota: number, DNI: number){
+    public constructor(nombre: string, nota: number, DNI: number) {
         this.nombre = nombre;
         this.nota = nota;
         this.DNI = DNI;
     }
 
-    public getNota(){
+    public getNota() {
         return this.nota;
     }
 
-    public estaAprobado(){
-        if(this.nota < 7){
+    public estaAprobado() {
+        if (this.nota < 7) {
             console.log("El alumno esta desaprobado")
         } else {
             console.log("El alumno esta aprobado")
@@ -28,99 +29,131 @@ class Alumno {
 class Profesor {
     private nombre: string;
     private DNI: number;
-    private listaAlumnos: Alumno[];
+    private listaAlumnos: Array<Alumno>;
 
-    public constructor(nombre: string, DNI: number, listaAlumnos: Alumno[]){
-        this.nombre= nombre;
-        this.DNI= DNI;
+    public constructor(nombre: string, DNI: number, listaAlumnos: Array<Alumno>) {
+        this.nombre = nombre;
+        this.DNI = DNI;
         this.listaAlumnos = listaAlumnos;
     }
 
-    public getListaAlumnos(){
+    public getListaAlumnos() {
         return this.listaAlumnos;
     }
 
+    public mostrarAlumnos() {
+        console.log(this.listaAlumnos)
+    }
+
+    public setListaAlumnos(arreglo: Array<Alumno>){
+        this.listaAlumnos = arreglo;
+    }
 
 }
 
-class Escuela{
+class Escuela {
     private nombre: string;
     private direccion: string;
-    private listaAlumnos: Alumno[];
-    private listaProfes: Profesor[];
+    private listaAlumnos: Array<Alumno>;
+    private listaProfes: Array<Profesor>;
 
-    public constructor(nombre:string, direccion: string, listaAlumnos: Alumno[], listaProfes: Profesor[]){
-        this.nombre= nombre;
-        this.direccion= direccion;
-        this.listaAlumnos =listaAlumnos;
-        this.listaProfes= listaProfes;
+    public constructor(nombre: string, direccion: string, listaAlumnos: Array<Alumno>, listaProfes: Array<Profesor>) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.listaAlumnos = listaAlumnos;
+        this.listaProfes = listaProfes;
     }
 
-    public getListaAlumnos(){
+    public getListaAlumnos() {
         return this.listaAlumnos;
     }
 
-    public getListaProfes(){
+    public getListaProfes() {
         return this.listaProfes;
     }
 
-    public setProfesor(profe : Profesor){
-        this.listaProfes.push(profe)
-    }
 }
 
+//instalar npm install @types/node
+// creamos un gestor que nos permite leer un archivo de texto
+
 class GestorDeArchivos {
+
     private arregloString: string[];
+
     constructor(txtFileLocation: string) {
-        let archivoTxt: string = fs.readFileSync(txtFileLocation, 'utf-8');
+
+        let archivoTxt: string = fs.readFileSync(txtFileLocation, 'utf-8'); //esta variable guarda "Juan Perez,333333333;Karen Simari,2222222"
         this.arregloString = archivoTxt.split(';');  //vamos a tener nuestro "objetos" separados por ;
+        //["Juan Perez,333333333","Karen Simari,22222222"]
+    
     }
+
     public mostrarArreglo(): void {
         console.log(this.arregloString);
     }
+
     public getArregloString(): string[] {
         return this.arregloString;
     }
+
 }
 
+//funcion para crear un nuevo profe
+
 function crearProfesor(profesor: string, arrayProfesor: Array<Profesor>, arrayAlumnos: Array<Alumno>): void {
+
     //transformo el elemento de tipo string en un objeto de tipo Profesor
-    let propiedadProfe = profesor.split(','); //[Juan Perez, 333333333, Karen Simari, 22222222]
+    let propiedadProfe : string[] = profesor.split(','); //la variable profesor va a contener --->"Juan Perez,333333333" y profesor.split(',') = ["Juan Perez", "333333333"]
     let nombre: string = propiedadProfe[0];
     let DNI: number = Number(propiedadProfe[1]);
     let listaAlumnos: Array<Alumno> = arrayAlumnos;
-
-    //creo el objeto nuevoProfesor de la clase Profesor y les paso sus atributos
     let nuevoProfe : Profesor = new Profesor(nombre,DNI,listaAlumnos);
+
     //inserto el elemento de tipo Profesor en el arreglo recibido
     arrayProfesor.push(nuevoProfe);
 }
 
 //Eliminar profesor en una posicion y agregar uno nuevo
 //instalamos readline-sync -- npm install readline-sync
+
 function borrarrProfe(arregloProfe: Array<Profesor>,  position: number, arregloAlumnos?: Array<Alumno>): void {
     //editar un profesor en el arreglo
-    let nombre: string = readlineSync.question("Ingrese el nombre: ")
-    let DNI: number = Number(readlineSync.question("Ingrese el DNI: "));
-    let listaAlumnos: Array<Alumno> = arrayAlumnos;
+    // let nombre: string = readlineSync.question("Ingrese el nombre: ")
+    // let DNI: number = Number(readlineSync.question("Ingrese el DNI: "));
+    // let listaAlumnos: Array<Alumno> = arregloAlumnos;
    
-    let nuevoProfe: Profesor = new Profesor(nombre,DNI,listaAlumnos);
+    // let nuevoProfe: Profesor = new Profesor(nombre,DNI,listaAlumnos);
+
     delete arregloProfe[position];
-    arregloProfe[position] = nuevoProfe;
+
+    // arregloProfe[position] = nuevoProfe;
  
 }
+
+
+
 //Inicio programa
-let datos: GestorDeArchivos = new GestorDeArchivos('profes.txt');
+let datos: GestorDeArchivos = new GestorDeArchivos('profes.txt'); // devuelve un arreglo de strings con "elementos" de profesores.
 let arrayProfe: Array<Profesor> = [];
+
 let alumno1 : Alumno = new Alumno('Gloria Dominguez', 8, 252546346);
 let alumno2 : Alumno = new Alumno('Juan Dominguez', 5, 25254255);
 alumno1.estaAprobado();
 alumno2.estaAprobado();
 let arrayAlumnos : Array<Alumno> = [alumno1,alumno2];
+
 //creo array de objetos
+
 for (let i: number = 0; i < datos.getArregloString().length; i++) {
+
     //Creo Profe uno por uno leyendo el txt
     
     crearProfesor(datos.getArregloString()[i], arrayProfe, arrayAlumnos);
+
 }
+
 console.log(arrayProfe);
+arrayProfe[1].mostrarAlumnos();
+
+
